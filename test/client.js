ad3f18ec61c03ff48927c54bd3502338ce66cc1d
vogdb <vozhdb@gmail.com>
2014-10-27 21:31:36 +0300

    Add test for an auxiliary server.

--- test/client.js
+++ test/client.js
@@ -9,36 +9,49 @@ var sinonChai = require("sinon-chai");
 chai.should();
 chai.use(sinonChai);
 
+function testJobExecution(pulsarApi, job, done) {
+  var createSpy = sinon.spy(function() {
+    job.data.status.should.equal(PulsarServerJob.STATUS.CREATED);
+    job.data.id.should.be.above(0);
+  });
+  job.on('create', createSpy);
+
+
+  var changeSpy = sinon.spy(function() {
+    createSpy.should.have.been.calledOnce;
+    job.data.status.should.equal(PulsarServerJob.STATUS.RUNNING);
+  });
+  job.on('change', changeSpy);
+
+  job.on('close', function() {
+    changeSpy.should.have.been.called;
+    job.data.status.should.equal(PulsarServerJob.STATUS.FINISHED);
+    done();
+  });
+
+  pulsarApi.runJob(job);
+}
+
 describe('tests of pulsar API', function() {
 
   this.timeout(4000);
 
-  it('Single instance', function(done) {
+  it('Default instance', function(done) {
     Helpers.createServer(config.single);
     var pulsarApi = new PulsarApi(config.single);
     var job = pulsarApi.createJob('app', 'env', 'task');
 
+    testJobExecution(pulsarApi, job, done);
+  });
 
-    var createSpy = sinon.spy(function() {
-      job.data.status.should.equal(PulsarServerJob.STATUS.CREATED);
-      job.data.id.should.be.above(0);
-    });
-    job.on('create', createSpy);
+  it('Auxiliary instance', function(done) {
+    var app = 'alice';
+    var env = 'production';
+    Helpers.createServer(config.multiple.auxiliary[app + '/' + env]);
+    var pulsarApi = new PulsarApi(config.multiple);
+    var job = pulsarApi.createJob(app, env, 'task');
 
-
-    var changeSpy = sinon.spy(function() {
-      createSpy.should.have.been.calledOnce;
-      job.data.status.should.equal(PulsarServerJob.STATUS.RUNNING);
-    });
-    job.on('change', changeSpy);
-
-    job.on('close', function() {
-      changeSpy.should.have.been.called;
-      job.data.status.should.equal(PulsarServerJob.STATUS.FINISHED);
-      done();
-    });
-
-    pulsarApi.runJob(job);
+    testJobExecution(pulsarApi, job, done);
   });
 
 });
