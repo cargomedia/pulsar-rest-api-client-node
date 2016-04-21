76346cb3486b4822251bf1ab7a96eda5b6916f49
vogdb <vozhdb@gmail.com>
2016-04-21 20:45:02 +0300

    Add Job tests

---
+++ test/job.js
@@ -0,0 +1,25 @@
+var assert = require("chai").assert;
+var Job = require('../src/job');
+var PulsarServerJob = require('pulsar-rest-api/lib/pulsar/job');
+
+describe('Job tests', function() {
+
+  var job;
+
+  beforeEach(function() {
+    job = new Job('app', 'env', 'task');
+  });
+
+  it('setData to .data', function() {
+    var data = {foo: 'bar'};
+    job.setData(data);
+    assert.deepEqual(job.data, data);
+  });
+
+  it('isRunning works', function() {
+    assert.isFalse(job.isRunning());
+    job.setData({status: PulsarServerJob.STATUS.RUNNING});
+    assert.isTrue(job.isRunning());
+  });
+
+});
