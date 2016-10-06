579c169b31798d5987a716c4861a485f30944cad
vogdb <vozhdb@gmail.com>
2016-10-06 22:22:30 +0300

    Rewrite Client tests

--- test/client.js
+++ test/client.js
@@ -1,108 +1,62 @@
 var _ = require('underscore');
-var config = require('./config');
-var Helpers = require('./helpers');
-var PulsarApi = require('../src');
-var PulsarServerJob = require('pulsar-rest-api/lib/pulsar/job');
-
-var chai = require("chai");
+var Promise = require('bluebird');
+var assert = require("chai").assert;
 var sinon = require("sinon");
-var sinonChai = require("sinon-chai");
-chai.should();
-chai.use(sinonChai);
+var Client = require('../src/client');
+var Job = require('../src/job');
 
-function testJobExecution(pulsarApi, job, done) {
-  var createSpy = sinon.spy(function() {
-    job.data.status.should.equal(PulsarServerJob.STATUS.CREATED);
-    job.data.id.should.be.above(0);
-  });
-  job.on('create', createSpy);
+describe('Client tests', function() {
 
+  context('runJob', function() {
 
-  var changeSpy = sinon.spy(function() {
-    createSpy.should.have.been.calledOnce;
-    job.data.status.should.equal(PulsarServerJob.STATUS.RUNNING);
-  });
-  job.on('change', changeSpy);
+    it('resolves if response contains job.id', function() {
+      var job = new Job('app', 'env', 'task');
+      var client = new Client('', '');
 
-  job.on('success', function() {
-    changeSpy.should.have.been.called;
-    job.data.status.should.equal(PulsarServerJob.STATUS.FINISHED);
-    done();
-  });
-
-  pulsarApi.runJob(job);
-}
-
-describe('tests of pulsar API', function() {
-
-  this.timeout(8000);
-  var server;
-
-  afterEach(function(done) {
-    if (server) {
-      server.close(function() {
-        done();
-        server = null;
+      sinon.stub(client._rest, 'request', function(url, options) {
+        assert.equal(url, '/app/env');
+        assert.include(options.body, {task: 'task'});
+        return Promise.resolve({id: 'id'});
       });
-    } else {
-      done();
-    }
-  });
 
-  it('Pulsar server gets correct params', function(done) {
-    server = Helpers.createServer(config.single);
-    var pulsarApi = new PulsarApi(config.single);
-    var job = pulsarApi.createJob('app', 'env', 'task', {key: 'value'});
-
-    server.on('job.create', function(data) {
-      var req = data.req;
-      req.params.app.should.equal('app');
-      req.params.env.should.equal('env');
-      req.body.task.should.equal('task');
-      req.body.taskVariables.should.be.a('object');
-      req.body.taskVariables.key.should.equal('value');
-      done();
+      return client.runJob(job);
     });
 
-    pulsarApi.runJob(job);
-  });
+    it('rejects if response does not contain job.id', function() {
+      var job = new Job('app', 'env', 'task');
+      var client = new Client('', '');
 
-  it('Default instance', function(done) {
-    server = Helpers.createServer(config.single);
-    var pulsarApi = new PulsarApi(config.single);
-    var job = pulsarApi.createJob('app', 'env', 'task');
+      sinon.stub(client._rest, 'request', function() {
+        return Promise.resolve({});
+      });
 
-    testJobExecution(pulsarApi, job, done);
-  });
-
-  it('Auxiliary instance', function(done) {
-    var app = 'alice';
-    var env = 'production';
-    server = Helpers.createServer(config.multiple.auxiliary[app + '/' + env]);
-    var pulsarApi = new PulsarApi(config.multiple);
-    var job = pulsarApi.createJob(app, env, 'task');
-
-    testJobExecution(pulsarApi, job, done);
-  });
-
-  it('Should reconnect to WebSocket server', function(done) {
-    server = Helpers.createServer(config.single);
-    var pulsarApi = new PulsarApi(config.single);
-    var job = pulsarApi.createJob('app', 'env', 'task');
-
-    var changeSpy = sinon.spy(function() {
-      job.data.status.should.equal(PulsarServerJob.STATUS.RUNNING);
-      server.getWebsocketServer().disconnectAllClients();
-    });
-    job.on('change', changeSpy);
-
-    job.on('success', function() {
-      changeSpy.should.have.been.called;
-      job.data.status.should.equal(PulsarServerJob.STATUS.FINISHED);
-      done();
+      return client.runJob(job)
+        .then(
+          function() {
+            throw new Error('client.runJob must fail in this test');
+          },
+          function(error) {
+            assert.include(error.message, ' id');
+          }
+        );
     });
 
-    pulsarApi.runJob(job);
   });
+
+  context('jobs', function() {
+    it('it directly returns response from url', function() {
+      var client = new Client('', '');
+
+      sinon.stub(client._rest, 'request', function(url) {
+        assert.equal(url, '/jobs');
+        return Promise.resolve('jobs');
+      });
+
+      return client.jobs().then(function(jobs) {
+        assert.equal(jobs, 'jobs');
+      });
+    });
+  });
+
 
 });
